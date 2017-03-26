57aee08edce63856826894859313a52dedc6f0bc
yzk2356911358 <2356911358@qq.com>
2017-03-26 16:53:54 +0800

    更改

--- src/test/java/com/yzk/carsystem/daoimp/UserDaoImpTest.java
+++ src/test/java/com/yzk/carsystem/daoimp/UserDaoImpTest.java
@@ -1,26 +1,26 @@
-//package com.yzk.carsystem.daoimp;
-//
-//import java.text.SimpleDateFormat;
-//import java.util.Calendar;
-//import java.util.Date;
-//
-//import org.junit.Test;
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.context.support.ClassPathXmlApplicationContext;
-//
-//import com.yzk.carsystem.dao.EmployeesDao;
-//import com.yzk.carsystem.model.CarMaster;
-//import com.yzk.carsystem.model.Employees;
-//
-//public class UserDaoImpTest {
-//	@SuppressWarnings("resource")
-//	@Test
-//	public void testModule() {
-//		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-//		EmployeesDao employeesDao = (EmployeesDao) applicationContext.getBean("employeesDaoImp");
-//		// Employees employees = new Employees(3, "小网", 500, currentTime);
-//		// employeesDao.updateEmployees(employees);
-//		CarMaster carMaster = new CarMaster();
-//		carMaster.getVip().getLevel();
-//	}
-//}
+package com.yzk.carsystem.daoimp;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.yzk.carsystem.dao.EmployeesDao;
+import com.yzk.carsystem.model.CarMaster;
+import com.yzk.carsystem.model.Employees;
+
+public class UserDaoImpTest {
+	@SuppressWarnings("resource")
+	@Test
+	public void testModule() {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		EmployeesDao employeesDao = (EmployeesDao) applicationContext.getBean("employeesDaoImp");
+		// Employees employees = new Employees(3, "小网", 500, currentTime);
+		// employeesDao.updateEmployees(employees);
+		CarMaster carMaster = new CarMaster();
+		carMaster.getVip().getLevel();
+	}
+}
